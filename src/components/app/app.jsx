c2135dd6c5f7ce9495c08f038c0b31baf81b29b6
alex <tte577es7@yandex.ru>
2020-10-03 20:54:15 +0400

    маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import Main from '../main/main.jsx';
 import PropTypes from 'prop-types';
 
+import Main from '../main/main.jsx';
+import SignIn from '../sign-in/sign-in.jsx';
+import Favorites from '../favorites/favorites.jsx';
+import Room from '../room/room.jsx';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 const App = (props) => {
   const {rentCount} = props;
 
   return (
-    <Main rentCount={rentCount} />
+    <Router>
+      <Switch>
+          <Route exact path="/login">
+            <SignIn />
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites />
+          </Route>
+          <Route exact path='/offer/:id' component={Room}>
+          </Route>
+          <Route exact path="/">
+            <Main rentCount={rentCount} />
+          </Route>
+        </Switch>
+    </Router>
   );
 };
 
