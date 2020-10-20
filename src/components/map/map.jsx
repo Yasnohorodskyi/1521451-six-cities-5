57e6045d9415978cdeb17ff5d30c5b69b4d2d098
alex <tte577es7@yandex.ru>
2020-10-20 21:53:44 +0300

    правки

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,78 +1,41 @@
 import React ,{PureComponent} from 'react';
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
+const city = [52.38333, 4.9];
 
 class MapCity extends PureComponent {
   constructor(props) {
     super();
-    this.state = {
-        city: {
-          lat: 52.3809553943508,
-          lng: 4.739309666406198,
-          zoom: 10,
-        },
-        idContainer: 'map',
-        layer: {
-          osmUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          minZoom: 8,
-          maxZoom: 30,
-          attribution:''
-        }
-    };
-
-
-
   }
   componentDidMount(){
-    const {
-      idContainer,
-      city,
-      layer
-    } = this.state;
 
-    const map = L.map(idContainer);
-
-    var myIcon = L.icon({
+    const icon = L.icon({
       iconUrl: `img/pin.svg`,
       iconSize: [30, 30]
     });
 
-    const osm = new L.TileLayer(
-      layer.osmUrl, {
-        minZoom: layer.minZoom,
-        maxZoom: layer.maxZoom,
-        attribution: layer.attribution
-      }
-    )
+    const zoom = 12;
+    const map = L.map(`map`, {
+      center: city,
+      zoom: zoom,
+      zoomControl: false,
+      marker: true
+    });
+    map.setView(city, zoom);
 
+    L.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  })
+  .addTo(map);
 
-
-    map.setView(
-      new L.LatLng(
-        city.lat,
-        city.lng
-      ),
-      city.zoom
-    );
-
-    map.addLayer(osm);
-    this.props.offers.forEach(element => {
-      let marker = L.marker([element.map.lat, element.map.lon],{icon: myIcon})
-      marker.bindPopup(element.title).openPopup();
-      marker.addTo(map)
-    })
+  const offerCords = [52.3709553943508, 4.89309666406198];
+  L.marker(offerCords, {icon}).addTo(map);
 
   }
   render() {
-    const position = [this.state.city.lat, this.state.city.lng];
     return (
       <div id='map'>
       <Map
-        center={[50, 10]}
-        zoom={40}
-        maxZoom={100}
-        attributionControl={true}
-        zoomControl={true}
         doubleClickZoom={true}
         scrollWheelZoom={true}
         dragging={true}
@@ -88,11 +51,6 @@ class MapCity extends PureComponent {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-          </Popup>
-        </Marker>
       </Map>
       </div>
     );
