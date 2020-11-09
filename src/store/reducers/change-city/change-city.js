ddd0173472fdbfb354af11546cc1387d445bc3fd
alex <tte577es7@yandex.ru>
2020-11-09 17:21:30 +0300

    УБрал лишнее

--- src/store/reducers/change-city/change-city.js
+++ src/store/reducers/change-city/change-city.js
@@ -1,26 +1,12 @@
 
 import {extend} from "../../../helpers/extend";
 
-const actionCity = {
-  CHANGE_CITY: `CHANGE_CITY`,
-  BASE_CITY: `Amsterdam`,
-};
 
-const stateCity = {
-  baseCity: actionCity.BASE_CITY,
-};
 
 
 export default function reducerCity(state = stateCity, action){
 
-  switch (action.type) {
-    case actionCity.CHANGE_CITY:
-      console.log(action);
-      return extend(state, {
-        currentCity: action.payload.title,
-        offers: action.payload.offers
-      });
-  }
+// Заготовка
 
   return state;
 };
--- src/store/reducers/filter-offers/filter-offers.js
+++ src/store/reducers/filter-offers/filter-offers.js
@@ -18,46 +18,9 @@ const converter = function (str) {
 };
 
 export default function reducerChangeOffers(state = stateOffers, action){
-  console.log(state);
-  console.log(action);
-  if(action.payload){
-    switch (action.payload.filter) {
-        case actionOffers.FILTER_POPULAR:
-          return extend(state, {
-            offers: state.offers,
-            baseFilter: action.payload.filter
-          });
-        case actionOffers.FILTER_TOP_RATED_FIRST:
-          action.payload.offers.sort(function (a, b) {
-            return b.rating - a.rating;
-          });
-          return extend(state, {
-            offers: state.offers,
-            baseFilter: action.payload.filter
-          });
-        case actionOffers.FILTER_PRICE_LOW_TO_HIGH:
-          console.log('FILTER_PRICE_LOW_TO_HIGH');
-          /*
-          action.payload.offers.sort(function (a, b) {
-            return a.price - b.price;
-          });
-          */
-          return extend(state, {
-            //offers: action.payload.offers,
-            baseFilter: action.payload.filter
-          });
-        case actionOffers.FILTER_PRICE_HIGH_TO_LOW:
-          action.payload.offers.sort(function (a, b) {
-            return converter(b.price) - converter(a.price);
-          });
 
-          return extend(state, {
-            offers: action.payload.offers,
-            baseFilter: action.payload.filter
-          });
-      }
-  }
+// Заготовка
 
 
       return state;
-  }
+}
